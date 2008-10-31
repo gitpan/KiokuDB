d3c633a41beebfba7abbfd643c19ac24a5c6e3f3
יובל קוג'מן (Yuval Kogman) <nothingmuch@woobling.org>
2008-10-31 17:13:47 +0000

    Import of NUFFIN/KiokuDB-0.07 from CPAN.
    gitpan-cpan-distribution: KiokuDB
    gitpan-cpan-version:      0.07
    gitpan-cpan-path:         NUFFIN/KiokuDB-0.07.tar.gz
    gitpan-cpan-author:       NUFFIN
    gitpan-cpan-maturity:     released

--- lib/KiokuDB.pm
+++ lib/KiokuDB.pm
@@ -3,7 +3,7 @@
 package KiokuDB;
 use Moose;
 
-our $VERSION = "0.06";
+our $VERSION = "0.07";
 
 use constant SERIAL_IDS => not not our $SERIAL_IDS;
 
@@ -613,7 +613,7 @@ referred to by another object, because that link will be broken.
 Executes $code within the scope of a transaction.
 
 This requires that the backend supports transactions
-(L<KiokuDB::Backend::TXN>).
+(L<KiokuDB::Backend::Role::TXN>).
 
 Transactions may be nested.
 
